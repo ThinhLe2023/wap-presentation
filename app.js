bb118060090d617e74543135860055f3ed7c443e
vien <viennv@newbees.jp>
2024-02-03 11:45:54 -0600

    comment connect DB

--- app.js
+++ app.js
@@ -19,12 +19,17 @@ app.use(express.urlencoded({ extended: false }));
 const mongoConnect = require('./util/database').mongoConnect;
 const Products = require('./models/product');
 
-mongoConnect(() => {
-    console.log('connected  =====');
-    app.listen(80, () => {
-        console.log('Your Server is running on 80');
-    })
-});
+
+app.listen(80, () => {
+    console.log('Your Server is running on 80');
+})
+
+// mongoConnect(() => {
+//     console.log('connected  =====');
+//     app.listen(80, () => {
+//         console.log('Your Server is running on 80');
+//     })
+// });
 
  console.log(Products.getAllProductByTitle('1'));
 // try to save product 
