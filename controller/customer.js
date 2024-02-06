ba3a0d6f64e47f656844e5ee0c7d731f84b180e5
Quoc Le <lequocthinh2010@gmail.com>
2024-02-06 11:50:52 -0600

    get products from DB

--- controller/customer.js
+++ controller/customer.js
@@ -1,13 +1,15 @@
 const Product = require('../models/product');
 
 exports.home = (req, res, next) => {
-    res.render('index', { path: '/', Product.getAllProduct() });
+
+    console.log(Product.getAllProduct() );
+    res.render('index', { path: '/', prods : Product.getAllProduct() });
 }
 
 exports.productFiler = (req, res, next) => {
     let category = req.params.category;
     let newProds = Product.getAllProductByCategory(category);
-    //console.log(listProduct, newProds, category);
+    console.log(listProduct, newProds, category);
     res.send(newProds);
 }
 
