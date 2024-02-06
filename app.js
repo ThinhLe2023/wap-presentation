167750c4c5230798e5044d74870268d656b82c3e
vien <viennv@newbees.jp>
2024-02-06 13:04:14 -0600

    add process for cookies and session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const session = require('express-session');
 const path = require('path');
 const multer = require('multer');
 const adminRouter = require('./router/adminRouter')
@@ -36,6 +37,7 @@ app.engine('html', ejs.renderFile);
 app.use('/images',express.static(path.join(__dirname, 'images')));
 
 app.use(cookieParser("usercookies"));
+app.use(session({secret: 'user-session', resave: false, saveUninitialized: false}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(multer({storage: fileStorege, fileFilter: fileFilter}).array('product_images')); //single('product_images'));//
