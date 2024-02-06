b61bd880430e60f589219d7f79bb0c5f66db40f9
vohoangxuan <hoangxuanvot@gmail.com>
2024-02-06 16:19:22 -0600

    update

--- controller/product.js
+++ controller/product.js
@@ -1,19 +1,37 @@
 let imgarr = ["/images/i15.jpeg", "/images/img3.jpeg", "/images/img4.jpeg", "/images/img5.jpeg"]
-const listProduct = require("../models/product.js")
+const db = require("../models/product.js")
+var ejs = require("ejs");
+let path = require('path');
 function getProduct (req, res, next) {
-    let id  = 1;
-    if(req.body.id)
-        id = parseInt(req.body.id);
-    let model = {id:1, imgarr: imgarr};
-    let i;
     
-    for (i = 0; i < listProduct.length; i++) {
-        
-        if(listProduct[i]._id == id) {
-            model = listProduct[i];
-            model.mainimg = listProduct[i].imageUrl;
+    let id  = '65c173e38cfa37f5178c670e';
+    if(req.body.id)
+        id = req.body.id;
+    let model = {id:1, imgarr: imgarr};
+
+    /*
+   db.getAllProduct().then(res=>{
+    console.log("getAllProduct================", res);
+   })
+   */
+    db.getProductById(id).then(result => {
+        //console.log("getProductById res_________", result);
+        if(result) {
+            //console.log("getProductById result_________", result);
+            model = result;
+            model.mainimg = "/images/i15.jpeg"; //result.imageUrl;
+            model.imgarr = imgarr;
+            let cart = getCartFromCookie(req, res);
+            model.noofitem = cart.reduce((accum, ele) => accum + ele.quantity, 0);
+            let subtotal = cart.reduce((accum, ele) => accum + parseFloat(ele.price)*ele.quantity, 0);
+            model.subtotal = subtotal;
+            model.cart = cart;
+            //console.log("model", model);
+            res.render("detail", model);
         }
-    }
+    });
+    /*
+    console.log("getProductById", model);
     model.imgarr = imgarr;
     //console.log("model=", model);
     let cart = [];
@@ -28,38 +46,72 @@ function getProduct (req, res, next) {
     model.subtotal = subtotal;
     console.log("model", model);
     res.render("detail", model);
+    */
 }
 
 function addToCart (req, res, next) {
-    console.log("ajax", req.query);
-    let cart = getCartFromCookie(req);
-    let id = parseInt(req.query.id);
+    //console.log("ajax", req.query);
+    let cart = getCartFromCookie(req, res);
+    let id = req.query.id;
 
     let i = findIndexOfProductId(cart, id);
     if(i != -1) {
         cart[i].quantity++;
+        res.cookie('cart', cart);
+        let model = {};
+        //console.log("addToCart cart", cart);
+        model.subtotal = cart.reduce((accum, ele) => accum + parseFloat(ele.price)*ele.quantity, 0);
+        model.noofitem = cart.reduce((accum, ele) => accum + ele.quantity, 0);
+        model.cart = cart;
+        //res.json(model);
+        renderCartHtml(res, model);
     } else {
-        let price = listProduct.find(ele=>ele._id=id).price;
-        cart.push({id:id, quantity:1, price:price});
-    }
+        db.getProductById(id).then(
+            //console.log("getProductById res_________", result);
+            function(val) { 
+                cart.push({id:id, quantity:1, price:parseFloat(val.price), title: val.title});
+                //console.log("Sau khi add vo cart", cart);
+                res.cookie('cart', cart);
+                console.log("addToCart cart getPrice", cart);
+                let model = {};
+                model.subtotal = cart.reduce((accum, ele) => accum + parseFloat(ele.price)*ele.quantity, 0);
+                model.noofitem = cart.reduce((accum, ele) => accum + ele.quantity, 0);
+                model.cart = cart;
 
-    console.log("Sau khi add vo cart", cart);
-    res.cookie('cart', cart);
-    res.cookie('test', 55);
-    let model = {};
-    model.subtotal = cart.reduce((accum, ele) => accum + ele.price*ele.quantity, 0);
-    model.noofitem = cart.reduce((accum, ele) => accum + ele.quantity, 0);
-    res.json(model);
+                renderCartHtml(res, model);
+                              
+            },
+            function(error) { /* code if some error */ }
+        );       
+    }    
 }
 
+function renderCartHtml(res, model) {
+    const renderHtmlFile = async () => {
+        try {
+            let html = await ejs.renderFile(path.join(__dirname, '../views/cartlist.html'), model);
+            //console.log(html);
+            return html;
+        } catch (error) {
+            console.log("Error occured: ", error);
+        }
+    }
+    let html = renderHtmlFile().then(
+        function(html) {
+            //console.log(html);
+            model.carthtml = html;
+            res.json(model);
+        }
+    );
+}
 
 function removeItem(req, res, next) {
-    console.log("ajax", req.query);
+    //console.log("ajax", req.query);
     let cart = [];
     let id = parseInt(req.query.id);
     if(req.cookies.cart) {
         cart = req.cookies.cart;  
-        console.log(req.cookies.cart);
+        //console.log(req.cookies.cart);
         let i = 0;
         for (i = 0; i < cart.length; i++) {
             if(cart[i].id == id) {
@@ -71,7 +123,7 @@ function removeItem(req, res, next) {
             res.cookie('cart', cart);
             let model = {};
             model.noofitem = cart.reduce((accum, ele) => accum + ele.quantity, 0);
-            model.subtotal = cart.reduce((accum, ele) => accum + ele.price*ele.quantity, 0);
+            model.subtotal = cart.reduce((accum, ele) => accum + parseFloat(ele.price)*ele.quantity, 0);
             res.json(model);
         }
     }     
@@ -79,7 +131,7 @@ function removeItem(req, res, next) {
 
 function changeQuantity(req, res, next) {
     let cart = [];
-    let id = parseInt(req.query.id);
+    let id = req.query.id;
     if(req.cookies.cart) {
         cart = req.cookies.cart;
         let idx = findIndexOfProductId(cart, id);
@@ -96,14 +148,14 @@ function changeQuantity(req, res, next) {
             }
             model.newquantity = item.quantity;
             model.noofitem = cart.reduce((accum, ele) => accum + ele.quantity, 0);
-            model.subtotal = cart.reduce((accum, ele) => accum + ele.price*ele.quantity, 0);;
+            model.subtotal = cart.reduce((accum, ele) => accum + parseFloat(ele.price)*ele.quantity, 0);;
             res.cookie('cart', cart);
             res.json(model);
         }
     }
 }
 
-function getCartFromCookie(req) {
+function getCartFromCookie(req, res) {
     let cart = [];
     if(req.cookies.cart) {
         cart = req.cookies.cart;
@@ -131,11 +183,11 @@ function getCart(req, res, next) {
         cart = req.cookies.cart;
     }
     let model = {};
-    cart.map(ele=>ele.title = (listProduct.find(p=>p._id=ele.id).title));
-    console.log("getcart", cart);
+    //cart.map(ele=>ele.title = (listProduct.find(p=>p._id=ele.id).title));
+    //console.log("getcart", cart);
     model.cart = cart;
     model.noofitem = cart.reduce((accum, ele) => accum + ele.quantity, 0);
-    model.subtotal = cart.reduce((accum, ele) => accum + ele.price*ele.quantity, 0);;
+    model.subtotal = cart.reduce((accum, ele) => accum + parseFloat(ele.price)*ele.quantity, 0);;
     res.render("cart", model);
 }
 
