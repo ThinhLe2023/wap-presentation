463951dbe9f3168f854d1aa4db6418eb7f522937
Quoc Le <lequocthinh2010@gmail.com>
2024-02-06 11:40:04 -0600

    Add product filter

--- controller/customer.js
+++ controller/customer.js
@@ -1,14 +1,16 @@
 const listProduct = require('../models/product');
 
 exports.home = (req, res, next) => {
+    console.log('here i am');
     res.render('index', { path: '/', prods: listProduct });
 }
 
 exports.productFiler = (req, res, next) => {
     let category = req.params.category;
     let newProds = listProduct.filter((item) => item.category == category);
-    //console.log(listProduct, newProds, category);
-    res.render('index', { path: '/filter/' + category, prods: newProds });
+    console.log(newProds);
+    res.send(newProds);
+    res.re
 }
 
 exports.adminHome = (req, res, next) => {
--- router/route.js
+++ router/route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const customer = require('../controller/customer');
 
-router.use('/filter/:category', customer.productFiler);
-router.use('/',customer.home);
+router.get('/filter/:category', customer.productFiler);
+router.get('/', customer.home);
 
 module.exports = router;
